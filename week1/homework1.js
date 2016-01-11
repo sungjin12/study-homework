8ffa123626487557fe71fb836408bcc0f15bb47a
egoing <egoing@gmail.com>
2016-01-12 01:04:59 +0900

    homework1 success

---
+++ week1/homework1.js
@@ -0,0 +1,37 @@
+var makeDiamond = function(num){
+	var star = '';
+	for(i = 0; i < num; i++){
+		for(j = 0; j < num - i - 1; j++){
+			star += ' ';
+		}
+		for(j = num - i - 1; j < num; j++){
+			for(k = 0; k < 2; k++){
+				star += '*';
+				if(j === num - i - 1){
+					break;
+				}
+			}
+		}
+		console.log(star);
+		star = '';
+	}
+	for(i = num - 1; i > 0; i--){
+		for(j = 0; j < num - i; j++){
+			star += ' ';
+		}
+		for(j = num - i; j < num; j++){
+			for(k = 0; k < 2; k++){
+				star += '*';
+				if(j === num - i){
+					break;
+				}
+			}
+		}
+		console.log(star);
+		star = '';
+	}
+}
+
+//makeDiamond(5);
+
+makeDiamond(3);

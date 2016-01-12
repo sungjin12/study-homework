f5e7c703cc5937300b6077718f462786bfb09eaf
egoing <egoing@gmail.com>
2016-01-12 18:14:48 +0900

    homework2 finish

--- week1/homework2.js
+++ week1/homework2.js
@@ -1,5 +1,12 @@
 var sumLineNum = function(num){
-
+	a = num+'';
+	sum = 0;
+	for(i = a.length; i > 0; i--){
+		b = Math.floor(num / Math.pow(10, i - 1));
+		num = num - b * Math.pow(10, i - 1);
+		sum += b;
+	}
+	console.log('각 자릿수의 총합 : ' + sum);
 }
 
 sumLineNum(54321);

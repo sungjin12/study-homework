e3468b1196705e736e764d7279802f8cf31881d1
egoing <egoing@gmail.com>
2016-01-17 19:32:43 +0900

    주석추가

--- week2/homework3.js
+++ week2/homework3.js
@@ -3,14 +3,12 @@ var getRandomNum = function(scope, num){
 	var print = scope + '개 숫자 중 랜덤한 숫자 ' + num + '개는\n';
 	var list = new Array();
 	var price = 0;
-	var list_size = 0;
 	var bool = true;
 
-	while(i < num) {
+	while(i < num) {                                   // 중복되는 값이 얼마나 나올지 모르기 떄문에 while로 반복문을 돌림
 		price = Math.floor(Math.random() * scope) + 1; // 랜덤한 값 구하기
 
-		list_size = list.length;
-		for(j = 0; j < list_size; j++) {                 // 리스트에 담긴 값중 새롭게 구한 랜덤값이 있는지 확인하여 없다면 넣기
+		for(j = 0; j < list.length; j++) {             // 중복되는 값 체크
 			if(list[j] === price) {
 				bool = false;
 			}

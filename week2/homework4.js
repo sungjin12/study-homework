61486da00735b7665a58f3c9b0415971627e8eff
egoing <egoing@gmail.com>
2016-01-17 19:44:13 +0900

    완성

--- week2/homework4.js
+++ week2/homework4.js
@@ -1,5 +1,14 @@
 var reversNum = function(num){
-	
+	var value = num;
+	var result = 0;
+
+	while(value > 0){
+		result *= 10;
+		a = value % 10;
+		value = Math.floor(value / 10);
+		result += a;
+	}
+	console.log(result);
 }
 
-reversNum();
+reversNum(123);

cb72f1744c8111a1ef27fe618118ac396f036649
egoing <egoing@gmail.com>
2016-01-17 17:31:41 +0900

    시간복잡도 줄이고 아래위로 나눠서 for문 돌린거 합치고 console.log를 하나만쓰게 만들고 ' '와 '*'을 쓰는 for문 합침

--- week1/homework1.js
+++ week1/homework1.js
@@ -1,37 +1,34 @@
 var makeDiamond = function(num){
 	var star = '';
-	for(i = 0; i < num; i++){
-		for(j = 0; j < num - i - 1; j++){
-			star += ' ';
-		}
-		for(j = num - i - 1; j < num; j++){
-			for(k = 0; k < 2; k++){
-				star += '*';
-				if(j === num - i - 1){
-					break;
+	var diamond_size = 2 * num - 1;
+	var mid = Math.floor(diamond_size/2);
+	for(i = 0; i < diamond_size; i++){
+		if(i <= mid){
+			for(j = 0; j < num; j++){
+				if(j < num - i - 1){
+					star += ' ';
+				} else if(j === num - i - 1){
+					star += '*';
+				} else {
+					star += '**';
+				}
+			}
+		} else {
+			for(j = 0; j < num; j++){   // 가운데를 넘어 갔기 때문에 총 길이에서 num만큼 빼줘 아래부분을 돌린다.
+				if(j < i - mid){   // 가운데를 넘어가면 i가 mid보다 커지는데 그 커지는 수만큼 빈칸을 만든다.
+					star += ' ';
+				} else if(j === i - mid){
+					star += '*';
+				} else {
+					star += '**';
 				}
 			}
 		}
-		console.log(star);
-		star = '';
+		star += '\n';
 	}
-	for(i = num - 1; i > 0; i--){
-		for(j = 0; j < num - i; j++){
-			star += ' ';
-		}
-		for(j = num - i; j < num; j++){
-			for(k = 0; k < 2; k++){
-				star += '*';
-				if(j === num - i){
-					break;
-				}
-			}
-		}
-		console.log(star);
-		star = '';
-	}
+	console.log(star);
 }
 
-makeDiamond(5);
+// makeDiamond(5);
 
 makeDiamond(3);
